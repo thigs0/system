a7be6fd6f68d3ddbb8150ac43861fba32eb6ba26
Thiago Santos <99839465+thigs0@users.noreply.github.com>
2023-07-03 00:27:55 -0300

    Update ubuntu.sh

--- sistema/ubuntu/ubuntu.sh
+++ sistema/ubuntu/ubuntu.sh
@@ -140,7 +140,39 @@ then
     sudo add-apt-repository ppa:graphics-drivers/ppa
     
     #instala minecraft
-    flatpak install flathub org.prismlauncher.PrismLauncher
+    #Instala Java
+          sudo apt install openjdk-8-jdk -y;
+          
+          #Instala o mine de fato
+          cd cd ~/Documentos/;
+          mkdir Tlaucher;
+          cd Tlaucher;
+          wget https://tlauncher.org/jar;
+          unzip jar;
+          rm READ*.txt;
+          rm jar;
+          
+          touch mine;
+          echo "
+          a="\$1";
+          if [ ""\$a"" = 'open' ] #Abre o Mine
+            then 
+              sudo java -jar ~/Documentos/Tlaucher/TLauncher-2.885.jar;
+          elif [ ""\$a"" = 'remove' ] #Remove o Mine do seu PC
+            then
+            sudo rm ~/Documentos/Tlaucher -r;
+            sudo rm /bin/mine;
+          elif [ ""\$a"" = 'help' ]
+            then
+            echo 'Escreva mine do terminal seguido da ação que queira, segue
+              a lista de comandos válidos:
+              mine open -> Para abrir o laucher
+              mine remove -> para excluir o mine do seu pc'
+          fi
+          " > mine;
+          
+          sudo mv mine /bin;
+          sudo chmod +x /bin/mine;
 
 elif [ ${mode} == 3]
 then
