450e0faf825d3ca42e9c03d6554f0b0185dbb672
Thigs <tthiagosantos38@gmail.com>
2023-10-27 14:50:46 -0300

    update

--- manjaro/code.sh
+++ manjaro/code.sh
@@ -13,9 +13,14 @@ sudo snap install julia --classic
 
 julia -e 'using Pkg; Pkg.add.(["CSV", "DataFrames", "Plots", "LinearAlgebra", "Javis", "Calculus", "PyPlot"])'
 
-
 # Instala latex
 sudo pacman -S texlive --noconfirm
 
 #instala git
 sudo pacman -S git --noconfirm
+
+sudo pacman -Sy neovim # instala neovim e lunarvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
+sudo snap install code --classic #Instala visual studio code
--- manjaro/essential.sh
+++ manjaro/essential.sh
@@ -2,3 +2,5 @@ sudo pacman -S flatpak --noconfirm
 
 pamac install snapd libpamac-snap-plugin
 sudo systemctl enable --now snapd.socket
+
+sudo pamac install dpkg
